e5cf134a014c02a0901174ff2e5bd0f1e2944e07
Muhammad Ali <139394180+RaeezAli@users.noreply.github.com>
2023-08-31 12:50:27 -0700

    Assignment No.01

---
+++ app.js
@@ -0,0 +1,90 @@
+//? CHAPTER N0.01
+
+//TODO Question No.1
+
+// alert("Assalamualaikum");
+
+//TODO Question No.2
+
+// alert("Error! Please enter a valid password");
+
+//TODO Question No.3
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+//TODO Question No.4
+
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding!\n▢ Prevent this page from creating additional dialogs.");
+
+//TODO Question No.5
+
+// document.querySelector("body").innerHTML= "<h1> Yah! I can write HTML content through JavaScript."
+
+// ! ************************************
+
+//? CHAPTER N0.02
+
+//TODO Question No.1
+
+// var userName;
+
+//TODO Question No.2
+
+// var myName;
+
+// myName = "Muhammad Ali";
+
+//TODO Question No.3
+
+// var message;
+
+// message = "Hello World";
+
+// alert(message);
+
+//TODO Question No.4
+
+// var studentName , studentAge , achievement ;
+
+// studentName = "Muhammad Ali"
+// studentAge = "18 year old"
+// achievement = "Web & App Development"
+
+// alert(studentName);
+// alert(studentAge);
+// alert(achievement);
+
+//TODO Question No.5 
+
+// var food;
+
+// food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+
+// alert(food);
+
+//TODO Question No.6
+
+// var email;
+
+// email = "muhammadali03@gmail.com";
+
+// alert("My email address is "+ email);
+
+//TODO Question No.7
+
+// var book;
+
+// book = "A smarter way to learn JavaScript";
+
+// alert("I am trying to learn from a Book "+book);
+
+//TODO Question No.8
+
+//  document.writeIn("Yah! I can write HTML content through JavaScript.");
+
+//TODO Question No.9
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
